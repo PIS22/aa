4b8fd45a75cb8432389e9bdee917c96cb2f63edc
Peter <ivedjossou@gmail.com>
2021-07-24 18:13:08 +0100

    Premier push

---
+++ src/main/java/com/Pas1Application.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pas1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pas1Application.class, args);
+	}
+
+}
